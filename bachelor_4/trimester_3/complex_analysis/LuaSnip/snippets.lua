d898472446338c84492cd76afb1e2eed937c4f90
Gaurish Sharma <sharmagaurish123@gmail.com>
2023-09-29 15:42:31 +1000

    dumping

--- bachelor_4/trimester_3/complex_analysis/LuaSnip/snippets.lua
+++
@@ -1,9 +0,0 @@
--- Include this `in_mathzone` function at the start of a snippets file...
-local in_mathzone = function()
-  -- The `in_mathzone` function requires the VimTeX plugin
-  return vim.fn['vimtex#syntax#in_mathzone']() == 1
-end
--- Then pass the table `{condition = in_mathzone}` to any snippet you want to
--- expand only in math contexts.
-return {
-}
